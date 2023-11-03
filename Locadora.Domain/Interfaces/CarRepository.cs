465d9ca75c006e23c54deeeb4a04823f232859ba
Ruan <ruan_araujo20@hotmail.com>
2023-11-03 12:17:58 -0300

    Atualizacao da interface

---
+++ Locadora.Domain/Interfaces/CarRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Locadora.Domain.Entities;
+
+namespace Locadora.Domain.Interfaces
+{
+    public interface CarRepository
+    {
+        void Create (Car car);
+        void Update (Car car);
+        Car GetBayId(Guid id,string brand);
+        Car DeleteBayId(Guid id);
+    
+
+    }
+}
